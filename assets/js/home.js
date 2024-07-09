51f032f575fa27be00e755333374481a2731be58
mayur-thinkitive <mayur.jadhav@thinkitive.com>
2024-07-09 20:40:38 +0530

    add education page

--- assets/js/home.js
+++ assets/js/home.js
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', function()
 
 
 
-educt
+
 function showFeature(featureNumber)
  {
   var allFeatures = document.querySelectorAll('.feature');
@@ -29,4 +29,41 @@ feature.style.display='none';
   {
     selectedFeature.style.display='block';
   }
+}
+
+
+// text-animation 
+
+const texts = ['Patient Care', 'Specialties', 'Billing', 'Practice'];
+let currentIndex = 0;
+
+function changeText() {
+    const textElement = document.getElementById('dynamicText');
+    textElement.style.animation = 'none';
+    void textElement.offsetWidth; 
+    textElement.innerHTML = texts[currentIndex];
+    textElement.style.animation = 'slideIn 3s forwards';
+    currentIndex = (currentIndex + 1) % texts.length;
+}
+
+setInterval(changeText, 2500);
+changeText(); 
+
+
+//text animation 
+
+
+
+function showFeature(featureNumber, element, containerId) {
+  var container = document.getElementById(containerId);
+  var features = container.querySelectorAll('.feature');
+  features.forEach(function(feature) {
+    feature.style.display = 'none';
+  });
+  container.querySelector('#feature' + featureNumber).style.display = 'block';
+  var links = container.querySelectorAll('.feature-links a');
+  links.forEach(function(link) {
+    link.classList.remove('active-link');
+  });
+  element.classList.add('active-link');
 }
